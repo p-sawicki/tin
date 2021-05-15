47be431c87b97e5489c0fb36860ef9efd681db4e
Marcin Białek <bialy.cinek@gmail.com>
2021-05-15 21:10:12 +0200

    logger update

--- logger/logger.py
+++ logger/logger.py
@@ -9,53 +9,32 @@ import os
 import re
 import csv
 import time
+import queue
 import traceback
+import threading
 from argparse import ArgumentParser
+from ipcqueue import posixmq
+from ipcqueue.serializers import RawSerializer
 
 
+running = True
 cpu_time_re = re.compile(r"cpu" + 10 * r"\s+(\d+)") 
 proc_stat_re = re.compile(r"(?:\S+ ){13}(\d+) (\d+) (?:\S+ ){7}(\d+)(?: \S+){29}")
 
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("-p", "--period", type=int, help="period in ms (default 1000)")
-    parser.add_argument("-l", "--logdir", type=str, help="log directory")
-    parser.add_argument("pids", type=int, nargs="+", help="process pids")
-    args = parser.parse_args()
-    period = args.period if (args.period and args.period > 0) else 1000
-    period = period / 1000
-    logdir = args.logdir or os.getcwd()
-
-    if any(p <= 0 for p in args.pids):
-        print("invalid pids")
-        exit(1)
-
+def logger_worker(proc_stack_lock: threading.Lock, proc_stack: list, period):
     try:
         last_total_time = 0
         stat_file = open("/proc/stat")
-        procs = []
 
-        for pid in args.pids:
-            proc_stat_file = open(f"/proc/{pid}/stat")
-            log_file = open(os.path.join(logdir, f"log_{pid}.csv"), "w")
-            csv_writer = csv.writer(log_file)
-            csv_writer.writerow(("time", "memory", "cpu_usage"))
-            procs.append({
-                "pid": pid,
-                "last_time": 0,
-                "stat_file": proc_stat_file,
-                "log_file": log_file,
-                "csv_writer": csv_writer
-            })
-
-        while True:
+        while running:
             total_time = sum([int(t) for t in cpu_time_re.match(stat_file.read()).groups()])
             dt = total_time - last_total_time
             stat_file.seek(0)
             current_time = int(1000000 * time.time())
+            proc_stack_lock.acquire(True)
 
-            for proc in procs:
+            for proc in proc_stack:
                 g = proc_stat_re.match(proc["stat_file"].read()).groups()
                 proc["stat_file"].seek(0)
                 t = int(g[0]) + int(g[1])
@@ -67,16 +46,85 @@ if __name__ == "__main__":
 
                 proc["last_time"] = t 
                 
+            proc_stack_lock.release()
             last_total_time = total_time
             time.sleep(period)
 
-    except KeyboardInterrupt:
-        ...
     except Exception:
         traceback.print_exc()
+
     finally:
         stat_file.close()
 
-        for proc in procs:
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-p", "--period", type=int, help="period in ms (default 1000)")
+    parser.add_argument("-l", "--logdir", type=str, help="log directory")
+    parser.add_argument("inqueue", type=str, help="input queue")
+    parser.add_argument("outqueue", type=str, help="output queue")
+    args = parser.parse_args()
+    period = args.period / 1000 if (args.period and args.period > 0) else 1
+    logdir = args.logdir or os.getcwd()
+
+    try:
+        input_queue = posixmq.Queue(args.inqueue, serializer = RawSerializer)
+        output_queue = posixmq.Queue(args.outqueue, serializer = RawSerializer)
+        proc_stack_lock = threading.Lock()
+        proc_stack = []
+        worker = threading.Thread(target = logger_worker, args = (proc_stack_lock, proc_stack, period))
+        worker.start()
+
+        while running:
+            msg = int.from_bytes(input_queue.get(), 'little', signed = True)
+
+            if msg >= 0:
+                proc_stat_file = open(f"/proc/{msg}/stat")
+                log_file = open(os.path.join(logdir, f"log_{msg}.csv"), "w")
+                csv_writer = csv.writer(log_file)
+                csv_writer.writerow(("time", "memory", "cpu_usage"))
+                proc_stack_lock.acquire(True)
+                proc_stack.append({
+                    "pid": msg,
+                    "last_time": 0,
+                    "stat_file": proc_stat_file,
+                    "log_file": log_file,
+                    "csv_writer": csv_writer
+                })
+                proc_stack_lock.release()
+                output_queue.put(msg.to_bytes(4, 'little', signed = True))
+                # print(f"Added process with pid {msg}")
+
+            elif msg == -1:
+                proc_stack_lock.acquire(True)
+
+                if len(proc_stack) > 0:
+                    proc = proc_stack.pop()
+                    proc_stack_lock.release()
+                    proc["stat_file"].close()
+                    proc["log_file"].close() 
+                    output_queue.put(int(0).to_bytes(4, 'little', signed = True))
+                    # print(f"Removed process with pid {proc['pid']}")
+                
+                else:
+                    proc_stack_lock.release()
+
+            elif msg == -2:
+                running = False 
+                worker.join()
+                # print("End")
+
+    except KeyboardInterrupt:
+        running = False 
+        worker.join()
+
+    except Exception:
+        traceback.print_exc()
+
+    finally:
+        input_queue.close()
+        output_queue.close()
+
+        for proc in proc_stack:
             proc["stat_file"].close()
             proc["log_file"].close() 
