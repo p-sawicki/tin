550b9ee13c4237eccd792aa8b8bcec3d4b8cb5f1
ubuntu <adam.solawa@gmail.com>
2021-05-22 14:09:38 +0200

    Add ssl to server

--- tcp/server.c
+++ tcp/server.c
@@ -5,9 +5,57 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
+
+void init_openssl()
+{ 
+    SSL_load_error_strings();	
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanup_openssl()
+{
+    EVP_cleanup();
+}
+
+SSL_CTX *create_context()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_server_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+	perror("Unable to create SSL context");
+	ERR_print_errors_fp(stderr);
+	exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx, const char* pem_cert, const char* pem_key)
+{
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+
+    /* Set the key and cert */
+    if (SSL_CTX_use_certificate_file(ctx, pem_cert, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+	exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, pem_key, SSL_FILETYPE_PEM) <= 0 ) {
+        ERR_print_errors_fp(stderr);
+	exit(EXIT_FAILURE);
+    }
+}
 
 // Function designed for chat between client and server.
 void func(int sockfd)
@@ -72,7 +120,7 @@ int server_loop(int sockfd) {
              perror("accept");
         else do {
              memset(buf, 0, sizeof buf);
-             if ((rval = read(msgsock,buf, 1024)) == -1)
+             if ((rval = read(msgsock,buf, 80)) == -1)
                  perror("reading stream message");
              if (rval == 0)
                  printf("Ending connection\n");
@@ -83,9 +131,16 @@ int server_loop(int sockfd) {
     } while(1);
 }
 
-int tcp_server(char const *server_name, int server_port) {
+int tcp_server(char const *server_name, int server_port,
+			   const char *pem_cert, const char *pem_key) {
   	printf("Starting tcp server on port %d\n", server_port);
 	int sockfd;
+	SSL_CTX *ctx;
+
+    init_openssl();
+    ctx = create_context();
+
+    configure_context(ctx, pem_cert, pem_key);
 
 	sockfd = create_server(server_name, server_port);
 
@@ -96,18 +151,18 @@ int tcp_server(char const *server_name, int server_port) {
 
 void usage(char const *name) {
   fprintf(stderr, "Usage:\n");
-  fprintf(stderr, "    %s ip_addr port\n", name);
+  fprintf(stderr, "    %s ip_addr port cert_file private_key_file\n", name);
   exit(1);
 }
 
 int main(int argc, char **argv) {
 	int exit_code = 0;
 
-	if (argc < 2) {
+	if (argc < 5) {
 		usage(argv[0]);
 	} else {
 		int server_port = atoi(argv[2]);
-		exit_code = tcp_server(argv[1], server_port);
+		exit_code = tcp_server(argv[1], server_port, argv[3], argv[4]);
 	}
 
   	exit(exit_code);
