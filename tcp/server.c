0e519d52461ae847aae8a61c7f4f461244fac7b8
ubuntu <adam.solawa@gmail.com>
2021-05-26 20:13:02 +0200

    Add logging to server

--- tcp/server.c
+++ tcp/server.c
@@ -1,36 +1,25 @@
+#include <stdint.h>
+#include <signal.h>
 #include "common.h"
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
 
+FILE *log_file;
+
 void configure_context(SSL_CTX *ctx, const char* pem_cert, const char* pem_key)
 {
-    SSL_CTX_set_ecdh_auto(ctx, 1);
+  SSL_CTX_set_ecdh_auto(ctx, 1);
 
-    /* Set the key and cert */
-    if (SSL_CTX_use_certificate_file(ctx, pem_cert, SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-		exit(EXIT_FAILURE);
-    }
+  if (SSL_CTX_use_certificate_file(ctx, pem_cert, SSL_FILETYPE_PEM) <= 0) {
+    ERR_print_errors_fp(log_file);
+    exit(EXIT_FAILURE);
+  }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, pem_key, SSL_FILETYPE_PEM) <= 0 ) {
-        ERR_print_errors_fp(stderr);
-		exit(EXIT_FAILURE);
-    }
-}
-
-// Function designed for chat between client and server.
-void func(int sockfd)
-{
-	char buff[MAX];
-	int n;
-	bzero(buff, MAX);
-
-	read(sockfd, buff, sizeof(buff));
-	printf("From client: %s\t");
-	bzero(buff, MAX);
-	n = 0;
-	// write(sockfd, buff, sizeof(buff));
+  if (SSL_CTX_use_PrivateKey_file(ctx, pem_key, SSL_FILETYPE_PEM) <= 0 ) {
+    ERR_print_errors_fp(log_file);
+    exit(EXIT_FAILURE);
+  }
 }
 
 int create_server(int server_port) {
@@ -38,11 +27,11 @@ int create_server(int server_port) {
 	struct sockaddr_in servaddr;
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
-		printf("socket creation failed...\n");
+		fprintf(log_file, "Socket creation failed...\n");
 		exit(0);
 	}
-	else
-		printf("Socket successfully created..\n");
+
+  fprintf(log_file, "Socket successfully created..\n");
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
@@ -50,26 +39,24 @@ int create_server(int server_port) {
 	servaddr.sin_port = htons(server_port);
 
 	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-		printf("socket bind failed...\n");
+		fprintf(log_file, "socket bind failed...\n");
 		exit(0);
 	}
 	else
-		printf("Socket successfully binded..\n");
+		fprintf(log_file, "Socket successfully binded..\n");
 
-    len = sizeof( servaddr);
-    if (getsockname(sockfd,(struct sockaddr *) &servaddr, &len)
-         == -1) {
-        perror("getting socket name");
-        exit(1);
-    }
-    printf("Socket port #%d\n", ntohs(servaddr.sin_port));
-	// Now server is ready to listen and verification
+  len = sizeof( servaddr);
+  if (getsockname(sockfd,(struct sockaddr *) &servaddr, &len) == -1) {
+    fprintf(log_file, "Getting socket name\n");
+    exit(1);
+  }
+  fprintf(log_file, "Socket port #%d\n", ntohs(servaddr.sin_port));
 	if ((listen(sockfd, 5)) != 0) {
-		printf("Listen failed...\n");
+		fprintf(log_file, "Listen failed...\n");
 		exit(0);
 	}
 	else
-		printf("Server listening..\n");
+		fprintf(log_file, "Server listening..\n");
 	return sockfd;
 }
 
@@ -77,11 +64,10 @@ void print_error_string(unsigned long err, const char* const label)
 {
     const char* const str = ERR_reason_error_string(err);
     if(str)
-        fprintf(stderr, "%s\n", str);
+      fprintf(log_file, "%s\n", str);
     else
-        fprintf(stderr, "%s failed: %lu (0x%lx)\n", label, err, err);
+      fprintf(log_file, "%s failed: %lu (0x%lx)\n", label, err, err);
 }
-#include <stdint.h>
 
 int server_loop(int sockfd, SSL_CTX *ctx, SSL *ssl1) {
 	int msgsock, rval;
@@ -93,18 +79,20 @@ int server_loop(int sockfd, SSL_CTX *ctx, SSL *ssl1) {
 	char buffer[SEGMENT_SIZE];
 	char *temp;
 	SSL *ssl;
+  
 	do {
-        msgsock = accept(sockfd,(struct sockaddr *) 0,(int *) 0);
-        if (msgsock == -1 )
-             perror("accept");
-        else do {
+    msgsock = accept(sockfd,(struct sockaddr *) 0,(int *) 0);
+    if (msgsock == -1 )
+      fprintf(log_file, "connection acceptin failed");
+    else do {
 			ssl = SSL_new(ctx);
-        	SSL_set_fd(ssl, msgsock);
+      SSL_set_fd(ssl, msgsock);
+
 			if (SSL_accept(ssl) <= 0) {
 				ssl_err = SSL_get_error(ssl, -1);
 				print_error_string(ssl_err, "Problem");
-            	ERR_print_errors_fp(stderr);
-        	} else {
+        ERR_print_errors_fp(log_file);
+      } else {
 				temp='1';
 				SSL_read(ssl, buffer, sizeof(char)*3);
 				temp = strtok(buffer, ",");
@@ -118,30 +106,29 @@ int server_loop(int sockfd, SSL_CTX *ctx, SSL *ssl1) {
 					}
 				}
 			}
-        } while (0);
-        SSL_free(ssl);
-        close(msgsock);
-    } while(1);
+    } while (0);
+    SSL_free(ssl);
+    close(msgsock);
+  } while(1);
 }
 
 int tcp_server(int server_port,
 			   const char *pem_cert, const char *pem_key) {
-  	printf("Starting tcp server on port %d\n", server_port);
+  fprintf(log_file, "Starting tcp server on port %d\n", server_port);
 	int sockfd;
 	SSL_CTX *ctx;
 	SSL *ssl;
 
-    init_openssl();
-    ctx = create_context(SERVER_CONTEXT);
-
-    configure_context(ctx, pem_cert, pem_key);
+  init_openssl();
+  ctx = create_context(SERVER_CONTEXT);
+  configure_context(ctx, pem_cert, pem_key);
 
 	sockfd = create_server(server_port);
 
 	server_loop(sockfd, ctx, ssl);
 
 	close(sockfd);
-    SSL_CTX_free(ctx);
+  SSL_CTX_free(ctx);
 }
 
 void usage(char const *name) {
@@ -150,15 +137,24 @@ void usage(char const *name) {
   exit(1);
 }
 
+void cleanup(int a)
+{
+  fclose(log_file);
+  exit(0);
+}
+
 int main(int argc, char **argv) {
 	int exit_code = 0;
 
 	if (argc < 4) {
 		usage(argv[0]);
 	} else {
+		log_file = open_log();
+    signal(SIGINT, cleanup);
+
 		int server_port = atoi(argv[1]);
 		exit_code = tcp_server(server_port, argv[2], argv[3]);
 	}
 
-  	exit(exit_code);
+  exit(exit_code);
 }
