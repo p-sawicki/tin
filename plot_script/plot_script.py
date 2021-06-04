834c6e4749b6669e2f99327e199e15b79275b7ad
Marcin Białek <bialy.cinek@gmail.com>
2021-06-04 22:01:57 +0200

    plot script update

--- plot_script/plot_script.py
+++ plot_script/plot_script.py
@@ -1,7 +1,11 @@
 import csv
 import numpy
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from datetime import datetime
+from argparse import ArgumentParser
+from itertools import zip_longest
 
 
 def readFile(filename):
@@ -11,16 +15,18 @@ def readFile(filename):
     """
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
+        next(reader) # skip header
+        time_t.append([])
+        mem_usage.append([])
+        cpu_usage.append([])
 
         for row in reader:
-            time_t.append(int(row[0]))
-            mem_usage.append(int(row[1]))
-            cpu_usage.append(int(row[2]))
-
-    return time_t, mem_usage, cpu_usage
+            time_t[-1].append(int(row[0]))
+            mem_usage[-1].append(int(row[1]))
+            cpu_usage[-1].append(float(row[2]))
 
 
-def plot(xlabel, ylabel, x, y):
+def plot(name, xlabel, ylabel, x, y):
     """
     drawing and saving plot
     """
@@ -33,20 +39,28 @@ def plot(xlabel, ylabel, x, y):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
-    plt.title(f'Wykres {ylabel} - {xlabel}\n'
+    plt.title(f'{name}\n'
               f'Średnia: {average:.2f} '
               f'Odchylenie standardowe: {std_deviation:.2f}')
-    plt.savefig(f'{act_time}_{ylabel}_{xlabel}.jpg', dpi=72)
+    plt.savefig(f'{act_time}_{name}.jpg', dpi=72)
     plt.clf()
 
 
 if __name__ == '__main__':
-
+    parser = ArgumentParser()
+    parser.add_argument("log_file", nargs="+", type=str, help="log files")
+    parser.add_argument("scenario", type=str, help="scenario name")
+    args = parser.parse_args()
     time_t = []
     mem_usage = []
     cpu_usage = []
+    
+    for filename in args.log_file:
+        readFile(filename)
 
-    time_t, mem_usage, cpu_usage = readFile('stat.csv')
-
-    plot('czas', 'zajętość pamięci', time_t, mem_usage)
-    plot('czas', 'zużycie procesora', time_t, cpu_usage)
+    time_t = max(time_t, key=len)
+    time_t = [(t - time_t[0])/1000 for t in time_t]
+    mem_usage = [(sum(v)/len(v))/1000000 for v in zip_longest(*mem_usage, fillvalue=0)]
+    cpu_usage = [sum(v)/len(v) for v in zip_longest(*cpu_usage, fillvalue=0)]
+    plot(f'{args.scenario}: średnie zużycie pamięci', 'czas [s]', 'zajętość pamięci [MB]', time_t, mem_usage)
+    plot(f'{args.scenario}: średnie zużycie procesora', 'czas [s]', 'zużycie procesora [%]', time_t, cpu_usage)
