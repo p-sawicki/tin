3b05a0976e098f467353393581dfdbf25067b436
Piotr Sawicki <sawickipiotr@outlook.com>
2021-06-07 18:50:03 +0200

    add headers to files in mvfst_impl

--- mvfst_impl/Client.h
+++ mvfst_impl/Client.h
@@ -1,3 +1,7 @@
+// TIN - Porównanie wybranych implementacji protokołów TCP i QUIC
+// Utworzono: 06.07.2021
+// Autor: Anna Pawlus
+
 #pragma once
 
 #include <iostream>
--- mvfst_impl/LogQuicStats.h
+++ mvfst_impl/LogQuicStats.h
@@ -1,10 +1,6 @@
-/*
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
+// TIN - Porównanie wybranych implementacji protokołów TCP i QUIC
+// Utworzono: 06.07.2021
+// Autor: Anna Pawlus
 
 #pragma once
 
